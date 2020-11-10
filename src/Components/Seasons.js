92808908a76857f1ba6c92fedeaed277de051dca
sepo21 <makefirst1@gmail.com>
2020-11-11 02:28:17 +0900

    fix error

--- src/Components/Seasons.js
+++ src/Components/Seasons.js
@@ -1,23 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
-import styled from "styled-components";
 import Section from "./Section";
 import Poster from "./Poster";
-
-const Container = styled.div`
-  margin-top: 30px;
-`;
-const Title = styled.span`
-  font-size: 17px;
-`;
-const List = styled.ul`
-  color: white;
-  margin-bottom: 50px;
-`;
-
-const Item = styled.li`
-  margin-top: 10px;
-`;
 
 const Seasons = ({ result }) => (
   <Section title="Seasons">
